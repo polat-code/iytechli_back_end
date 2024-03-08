3b268c1d04d9439295e334fbb7d99d3f7a3e72f0
Özgürhan Polat <ozgurhan.45@gmail.com>
2024-03-08 09:57:06 +0300

    message classes are deleted

--- src/main/java/com/example/iytechli/message/domain/model/entity/UserType.java
+++
@@ -1,6 +0,0 @@
-package com.example.iytechli.message.domain.model.entity;
-
-public enum UserType {
-    CLIENT,
-    CROSS_CLIENT
-}
--- src/main/java/com/example/iytechli/message/repository/MessageDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.iytechli.message.repository;
-
-import com.example.iytechli.message.domain.model.entity.MessageDetail;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MessageDetailRepository extends MongoRepository<MessageDetail,String> {
-}
